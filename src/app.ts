6ff274dd507d62bdfa5e67bfda9e8fec590e2349
kassioba <kassioluna@gmail.com>
2023-10-06 03:13:13 -0300

    fix: trust proxy

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ import axios from 'axios';
 
 const app = express();
 
+app.set('trust proxy', true);
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
