7c21eb1b068ce8aa43bbea4fb857b006620888df
kassioba <kassioluna@gmail.com>
2023-10-09 05:33:15 -0300

    fix: trust proxy error

--- src/app.ts
+++ src/app.ts
@@ -8,20 +8,20 @@ const app = express();
 
 loadEnv()
 
-app.set('trust proxy', true);
+// app.set('trust proxy', true);
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
-  standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-  message: "Too many requests, please try again later",
-});
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 minutes
+//   max: 100, // limit each IP to 100 requests per windowMs
+//   standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+// 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+//   message: "Too many requests, please try again later",
+// });
 
 app.use(cors())
 .use(express.json())
 .use(router)
-.use(limiter)
+// .use(limiter)
 
 app
 .get("/health", (req: Request, res: Response) => res.send("I'm okay!")) 
