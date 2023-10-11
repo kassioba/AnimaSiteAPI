814bcc94865f4647421b3b75b2d0a7258725264c
kassioba <kassioluna@gmail.com>
2023-10-11 02:26:50 -0300

    refactor: cep joi validation with regex

--- src/schemas/cep.schema.ts
+++ src/schemas/cep.schema.ts
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 const cepSchema = Joi.object({
-    cep: Joi.string().min(8).max(9).required()
+    cep: Joi.string().pattern(/^[0-9]{5}-?[0-9]{3}$/).required()
 })
 
 export default cepSchema
